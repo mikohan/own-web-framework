152439316a969822527be1d78cbdeb65644f8054
mikohan <angara99@gmail.com>
2020-11-22 12:31:46 +0300

    Added concurrently and json-server

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -223,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42811" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43509" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
