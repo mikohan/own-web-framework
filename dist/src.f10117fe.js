53bf637f241f38d3217e046776bb19b086c4fa05
mikohan <angara99@gmail.com>
2020-11-23 14:56:50 +0300

    Finish event system

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -120,6 +120,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"src/views/UserForm.ts":[function(require,module,exports) {
 "use strict";
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -142,7 +154,8 @@ var UserForm = /*#__PURE__*/function () {
     key: "eventsMap",
     value: function eventsMap() {
       return {
-        'click:button': this.onButtonClick
+        'click:button': this.onButtonClick,
+        'mouseenter:h4': this.onHeaderHover
       };
     }
   }, {
@@ -151,15 +164,41 @@ var UserForm = /*#__PURE__*/function () {
       console.log('Hi there');
     }
   }, {
+    key: "onHeaderHover",
+    value: function onHeaderHover() {
+      console.log('Hovered h1 title');
+    }
+  }, {
     key: "template",
     value: function template() {
-      return "\n    <div>\n    <h1>User Form</h1>\n    <input />\n    </div>\n    ";
+      return "\n    <div>\n    <h4>User Form</h4>\n    <input />\n    <button>Click Me</button>\n    </div>\n    ";
+    }
+  }, {
+    key: "bindEvents",
+    value: function bindEvents(fragment) {
+      var eventsMap = this.eventsMap();
+
+      var _loop = function _loop(eventKey) {
+        var _eventKey$split = eventKey.split(':'),
+            _eventKey$split2 = _slicedToArray(_eventKey$split, 2),
+            eventName = _eventKey$split2[0],
+            selector = _eventKey$split2[1];
+
+        fragment.querySelectorAll(selector).forEach(function (element) {
+          element.addEventListener(eventName, eventsMap[eventKey]);
+        });
+      };
+
+      for (var eventKey in eventsMap) {
+        _loop(eventKey);
+      }
     }
   }, {
     key: "render",
     value: function render() {
       var templateElement = document.createElement('template');
       templateElement.innerHTML = this.template();
+      this.bindEvents(templateElement.content);
       this.parent.append(templateElement.content);
     }
   }]);
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,25 +4,44 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseenter:h4': this.onHeaderHover,
     };
   }
 
   onButtonClick(): void {
     console.log('Hi there');
   }
+  onHeaderHover(): void {
+    console.log('Hovered h1 title');
+  }
 
   template(): string {
     return `
     <div>
-    <h1>User Form</h1>
+    <h4>User Form</h4>
     <input />
+    <button>Click Me</button>
     </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+
     this.parent.append(templateElement.content);
   }
 }
