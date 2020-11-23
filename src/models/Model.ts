74e03ad7b26f54b90a97fb896f334b14fd58e33f
mikohan <angara99@gmail.com>
2020-11-23 09:37:31 +0300

    Done interfaces for model

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,7 +1,19 @@
-export interface IModelAttributes {}
+import { AxiosPromise } from 'axios';
 
-export interface ISync {}
+export interface IModelAttributes<T> {
+  set(value: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
 
-export interface IEventns {}
+export interface ISync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+export interface IEventns {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
 
 export class Model {}
